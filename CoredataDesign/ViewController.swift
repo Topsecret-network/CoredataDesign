b01e80d00396d6cd738e2e7490dfba79da59e013
chaos <lucas@keepsecret.io>
2023-07-27 10:19:00 +0800

    Initial Commit

---
+++ CoredataDesign/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoredataDesign
+//
+//  Created by chaos on 2023/7/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
